b4710743e0d1aacfcebc82f80242970d5466f424
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2026-07-05 20:43:06 -0300

    feat: add scroll on screens

--- app/src/main/java/br/com/schmittsolucoes/cacainfinita/presentation/home/composables/HomeScreen.kt
+++ app/src/main/java/br/com/schmittsolucoes/cacainfinita/presentation/home/composables/HomeScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -55,6 +57,7 @@ fun HomeScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
+                .verticalScroll(rememberScrollState())
                 .padding(horizontal = 12.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
--- app/src/main/java/br/com/schmittsolucoes/cacainfinita/presentation/puzzles/composables/components/AddWordSearchBottomSheet.kt
+++ app/src/main/java/br/com/schmittsolucoes/cacainfinita/presentation/puzzles/composables/components/AddWordSearchBottomSheet.kt
@@ -13,7 +13,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -49,6 +51,7 @@ fun AddWordSearchBottomSheet(
         Column(
             modifier = Modifier
                 .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
                 .padding(bottom = 32.dp, start = 24.dp, end = 24.dp)
         ) {
             Text(
